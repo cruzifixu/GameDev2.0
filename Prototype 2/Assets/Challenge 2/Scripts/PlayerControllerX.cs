5c5e99eabf21060f16ff61012419f01901bbc8fd
cruzifixu <franchezcacruz09@gmail.com>
2022-03-09 16:43:30 +0100

    Bugfix Challenge done no extras

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timer = 0;
+    private float breaktTime = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime; // counter to prevent from spamming
+        if(timer >= breaktTime)
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timer = 0;
+            }
+        }
+    }
+}
